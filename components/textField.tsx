6384043cf7270c0f3fc061fcb217be51ed39acb4
yash <yashraj.ch2048@gmail.com>
2024-10-25 18:45:35 +0530

    Refactor TextField component to display a cross icon when selected
    - Modify TextField component to import Image and TouchableOpacity from react-native
    - Add selectContext import from '../context/selectedContext'
    - Add CrossIcon image import from '../assests/cross_icon.png'
    - Add isSelected and selectActive variables from selectContext
    - Add TouchableOpacity component with CrossIcon image when isSelected is true
    - Update styles to display the cross icon button

--- components/textField.tsx
+++ components/textField.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { useContext } from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
 
+import {selectContext} from '../context/selectedContext';
+const CrossIcon = require('../assests/cross_icon.png');
 
 
 export default function TextField({fileName}:{fileName:string|null}) {
+  const context = useContext(selectContext);
+
+  const {isSelected, selectActive} = context;
+
   return (
     <View style={styles.field}>
       <Text style={styles.text}>{fileName}</Text>
+     { isSelected && <TouchableOpacity style={styles.button}>
+        <Image source={CrossIcon} style={styles.image} />
+      </TouchableOpacity>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   field: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: '70%',
     height: '65%',
     borderColor: '#b0b0b0',
@@ -25,8 +37,24 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'rgb(0, 0, 0)',
+    borderColor: '#b0b0b0',
+    borderWidth: 1.2,
+    marginLeft: 5,
     textAlign: 'left',
     marginVertical: 'auto',
-    paddingLeft: 5,
+
+  },
+  button: {
+    marginVertical: 'auto',
+    marginRight: 5,
+    backgroundColor: '#ffffff',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: 'red',
+  },
+  image: {
+    width: 30,
+    height: 30,
   },
 });
