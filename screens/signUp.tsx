ac9068c48823563ccdf2d37d67d1039734fda70c
Yash <yashraj.ch2048@gmail.com>
2024-11-11 16:37:16 +0530

    installed use form react library Added basic login and sign-up forms without functionality

--- screens/signUp.tsx
+++ screens/signUp.tsx
@@ -1,21 +1,68 @@
-import {ScrollView, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
+import {ScrollView, StyleSheet, TextInput} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
 
 import Header from '../components/header';
+import RectangularButton from '../components/rectangularButton';
+
 export default function SignUp() {
+  const {control, handleSubmit} = useForm();
+
+  const onSubmit = data => {
+    console.log('Form Data:', data); // Handle form data here
+  };
+
   return (
-    <SafeAreaView style={styles.superContainer}>
+    <>
       <Header />
-      <ScrollView style={styles.canvas} />
-    </SafeAreaView>
+      <ScrollView style={styles.canvas}>
+        <Controller
+          name="username"
+          control={control}
+          rules={{required: 'Username is required'}}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          rules={{required: 'Password is required'}}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        <RectangularButton text={'SignUp'} onPress={handleSubmit(onSubmit)} />
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   canvas: {
     backgroundColor: '#caf0f8',
+    padding: 16,
   },
-  superContainer: {
-    flex: 1,
+  input: {
+    backgroundColor: 'white',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: 12,
   },
 });
