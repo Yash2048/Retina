e1266c18025ab2d0c9918abfbe07076a88cbea9e
yash <yashraj.ch2048@gmail.com>
2024-12-01 02:02:25 +0530

    gave the header and footer dynamic height and styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,16 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import React, {useState} from 'react';
 import FilesButton from './filesButton';
 import TextField from './textField';
 import {DocumentPickerResponse} from 'react-native-document-picker';
 
-
 export default function Footer() {
+  const {height, width} = useWindowDimensions();
+  const passedHeight = height > width ? height * 0.1 : width * 0.09;
+  const styles = dynamicSytles(passedHeight);
+
   const [video, setVideo] = useState<DocumentPickerResponse | null>(null);
-  const [fileName, setFileName] = React.useState<string|null>('');
+  const [fileName, setFileName] = React.useState<string | null>('');
 
   return (
     <View style={styles.footer}>
@@ -17,10 +20,11 @@ export default function Footer() {
   );
 }
 
-const styles = StyleSheet.create({
-  footer: {
-    flexDirection:'row',
-    backgroundColor: '#023047',
-    height: '9%',
-  },
-});
+const dynamicSytles = (height: number) =>
+  StyleSheet.create({
+    footer: {
+      flexDirection: 'row',
+      backgroundColor: '#023047',
+      height: height,
+    },
+  });
--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,10 @@
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, useWindowDimensions} from 'react-native';
 import React from 'react';
 
 export default function Footer() {
+  const {height, width} = useWindowDimensions();
+  const passedHeight = height > width ? height * 0.1 : width * 0.08;
+  const styles = dynamicSytles(passedHeight);
   return (
     <View style={styles.header}>
       <Text style={styles.title}>Retina</Text>
@@ -9,15 +12,16 @@ export default function Footer() {
   );
 }
 
-const styles = StyleSheet.create({
-  header: {
-    backgroundColor: '#023047',
-    height: '9%',
-  },
-  title: {
-    color: 'white',
-    margin: 'auto',
-    fontSize: 38,
-    fontFamily:'OpenSans-Light',
-  },
-});
+const dynamicSytles = (height: number) =>
+  StyleSheet.create({
+    header: {
+      backgroundColor: '#023047',
+      height: height,
+    },
+    title: {
+      color: 'white',
+      margin: 'auto',
+      fontSize: 38,
+      fontFamily: 'OpenSans-Light',
+    },
+  });
