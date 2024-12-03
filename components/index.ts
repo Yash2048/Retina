45804f662d2bc19354aa57cc845faeb51c42a22a
yash <yashraj.ch2048@gmail.com>
2024-12-03 19:31:31 +0530

    Added index.ts for components folder.

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export {default as Header} from './header';
+export {default as Footer} from './footer';
+export {default as RectangularButton} from './rectangularButton';
+export {default as TextField} from './textField';
+export {default as LoadingBar} from './loadingBar';
+export {default as FilesButton} from './filesButton';
